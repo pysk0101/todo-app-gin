86120e6b56722ebd92cb2bb15bc6a0773261f6a0
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-07-09 23:38:27 +0300

    here is server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database-connection';
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -11,17 +12,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.use(express.json())
+app.use(cookieParser());
 
 //Database
 connectDB();
 
 //Routes
+import registerRouter from './routes/registerRouter';
+import authRouter from './routes/authRouter'; 
 import todoRouter from './routes/todoRouter';
 import userRouter from './routes/userRouter';
+import pageRouter from './routes/pageRouter';
 
 //APIs
+app.use('/signup', registerRouter);
+app.use('/login', authRouter);
 app.use('/todo', todoRouter);
 app.use('/user', userRouter);
+app.use("/", pageRouter)
 
 
 //Port
