6ce24109ec2b7ba93e20e6fb447000872f3d5014
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-07-11 22:28:46 +0300

    tag operations added

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,7 +1,10 @@
 import express from "express"
 const router = express.Router()
-import  {createUser} from "../controllers/userController" 
-router.post("/", createUser)
+
+import  {getUser, createTag,deleteTag} from "../controllers/userController" 
+import verifyJWT from "../middlewares/verifyJWT";
+
+router.delete("/deleteTag", deleteTag).get("/:userId",verifyJWT, getUser).post("/addTag", createTag)
 
 
 export default router
