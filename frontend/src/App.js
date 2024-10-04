bfffdf951aea4ad834a69f06eb32293d0e52872a
Abdulrahman Sa <c.abdulrahmansaka@gmail.com>
2024-10-04 16:58:03 +0300

    adding tailwind and packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/footer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App ">
+      <Header />
+      <List />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header>
+      <h1 className="bg-black">hello</h1>
+    </header>
+  );
 }
