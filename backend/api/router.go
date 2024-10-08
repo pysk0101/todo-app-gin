42955c58a60e39c3b657df929991f68e6c90d52b
pysk0101 <kucukaslanmuhammed852@gmail.com>
2024-10-08 17:53:26 +0300

    router.go created and added todo routes

--- backend/api/router.go
+++ backend/api/router.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pysk0101/todo-app-gin/backend/pkg/todos"
+)
+
+func SetupRoutes(r *gin.Engine, todoHandler *todos.TodoHandler) {
+	todoGroup := r.Group("/todos")
+	{
+		todoGroup.GET("", todoHandler.FindAll)       // /todos
+		todoGroup.POST("", todoHandler.Create)       // /todos
+		todoGroup.PUT("", todoHandler.Update)        // /todos/:id
+		todoGroup.DELETE("/:id", todoHandler.Delete) // /todos/:id
+	}
+}
