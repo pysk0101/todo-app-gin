0972cb6591a3c1a2e0c706d31ce6a15ea07ca165
Abdulrahman Sa <c.abdulrahmansaka@gmail.com>
2024-10-09 20:51:00 +0300

    adding header and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Lists from "./components/Lists";
 
 function App() {
-  return <div className={` App `}></div>;
+  return (
+    <div className={`App bg-main min-h-[100svh] `}>
+      <Header />
+      <Lists />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Header/Navbar.jsx
@@ -0,0 +1,97 @@
+import { MdDarkMode } from "react-icons/md";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Dashboard", href: "#", current: false },
+  { name: "Login", href: "#", current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar() {
+  return (
+    <Disclosure as="nav" className="bg-secondary dark:bg-darkSecondary">
+      <div className="max-w-full px-2 mx-auto sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="relative inline-flex items-center justify-center p-2 rounded-md text-text dark:text-darkText group hover:bg-primary50 hover:dark:bg-darkPrimary50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary dark:focus:ring-darkPrimary">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block h-6 w-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden h-6 w-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+            <div className="flex items-center flex-shrink-0">
+              <img alt="Your Company" src="./Logo.png" className="w-auto h-8" />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-primary text-secondary dark:bg-darkPrimary dark:text-darkSecondary"
+                        : "text-text dark:text-darkText hover:bg-primary dark:hover:bg-darkPrimary hover:text-secondary dark:hover:text-darkSecondary",
+                      "rounded-md px-3 py-2 text-sm font-medium transition "
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button
+              type="button"
+              className="relative p-1 transition rounded-full text-primary dark:text-darkPrimary bg-main dark:bg-darkMain hover:opacity-75 hover:scale-105"
+            >
+              <span className="absolute -inset-1.5" />
+              <MdDarkMode aria-hidden="true" className="w-8 h-8" />
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-primary text-secondary dark:bg-darkPrimary dark:text-darkSecondary"
+                  : "text-text dark:text-darkText hover:bg-primary dark:hover:bg-darkPrimary hover:text-secondary dark:hover:text-darkSecondary",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+}
